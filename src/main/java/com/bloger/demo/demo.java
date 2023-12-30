beae4513675877cb8faab7a23af5adbb74d1ac7c
ncd405 <ngodatwin2@gmail.com>
2023-12-31 00:03:49 +0700

    Upload Project Bloger

---
+++ src/main/java/com/bloger/demo/demo.java
@@ -0,0 +1,17 @@
+package com.bloger.demo;
+
+public class demo {
+    public static void main(String[] args) {
+        // In ra màn hình mà không xuống dòng
+        System.out.print("Hello, ");
+
+        // In ra màn hình và xuống dòng
+        System.out.println("World!");
+
+        // Có thể kết hợp nhiều giá trị hoặc biểu thức
+        int x = 5;
+        System.out.println("The value of x is: " + x);
+    }
+
+
+}
---
+++ src/test/java/com/bloger/app/BlogerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloger.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
